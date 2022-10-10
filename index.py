5e6ceaf3335058edc6ff29a148825e4a316c1d88
looongtom <94033356+looongtom@users.noreply.github.com>
2022-10-11 00:06:32 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,11 @@
 from tkinter import *
 from tkinter import messagebox
+from PIL import ImageTk,Image
 import pygame
+
+
 def start_main_page():
+    
     def start_game(args):
         main_window.destroy()
         if args == 1:
@@ -25,33 +29,37 @@ def start_main_page():
         elif args == 7:
             from Options import Vehicles
             Vehicles.main()
-
-    def option():
-
+    
+    
+    def option():    
         lab_img1 = Button(
             text="CHỌN CHỦ ĐỀ",
-            bg='#7f9dc0',
-            border=0,
-            justify='left',
-            font=("Arial", 18)
+            bg='#16D3E0',
+            border=5,
+
+            justify='right',
+            font=("Kristen ITC", 20)
 
         )
+        
+       
         sel_btn1 = Button(
-            text="ĐỘNG VẬT",
-            width=18,
-            borderwidth=8,
-            font=("", 18),
-            fg="#000000",
-            bg="#dfa801",
-            cursor="hand2",
-            command=lambda: start_game(1),
+            
+        text="ĐỘNG VẬT",
+        width=13,
+        borderwidth=8,
+        font=("", 15),
+        fg="#000000",
+        bg="#dfa801",
+        cursor="hand2",
+        command=lambda: start_game(1),
         )
-
+        
         sel_btn2 = Button(
             text="CƠ THỂ",
-            width=18,
+            width=13,
             borderwidth=8,
-            font=("", 18),
+            font=("", 15),
             fg="#000000",
             bg="#dfa801",
             cursor="hand2",
@@ -60,9 +68,9 @@ def start_main_page():
 
         sel_btn3 = Button(
             text="MÀU SẮC",
-            width=18,
+            width=13,
             borderwidth=8,
-            font=("", 18),
+            font=("", 15),
             fg="#000000",
             bg="#dfa801",
             cursor="hand2",
@@ -71,9 +79,9 @@ def start_main_page():
 
         sel_btn4 = Button(
             text="HOA QUẢ",
-            width=18,
+            width=13,
             borderwidth=8,
-            font=("", 18),
+            font=("", 15),
             fg="#000000",
             bg="#dfa801",
             cursor="hand2",
@@ -82,9 +90,9 @@ def start_main_page():
 
         sel_btn5 = Button(
             text="HÌNH KHỐI",
-            width=18,
+            width=13,
             borderwidth=8,
-            font=("", 18),
+            font=("", 15),
             fg="#000000",
             bg="#dfa801",
             cursor="hand2",
@@ -93,9 +101,9 @@ def start_main_page():
 
         sel_btn6 = Button(
             text="RAU CỦ",
-            width=18,
+            width=13,
             borderwidth=8,
-            font=("", 18),
+            font=("", 15),
             fg="#000000",
             bg="#dfa801",
             cursor="hand2",
@@ -104,77 +112,153 @@ def start_main_page():
 
         sel_btn7 = Button(
             text="XE CỘ",
-            width=18,
+            width=13,
             borderwidth=8,
-            font=("", 18),
+            font=("", 15),
             fg="#000000",
             bg="#dfa801",
             cursor="hand2",
             command=lambda: start_game(7),
         )
-        lab_img1.grid(row=0, column=0, padx=20)
-        sel_btn1.grid(row=0, column=4, pady=(10, 0), padx=0, )
-        sel_btn2.grid(row=1, column=4, pady=(10, 0), padx=0, )
-        sel_btn3.grid(row=2, column=4, pady=(10, 0), padx=0, )
-        sel_btn4.grid(row=3, column=4, pady=(10, 0), padx=0, )
-        sel_btn5.grid(row=4, column=4, pady=(10, 0), padx=0, )
-        sel_btn6.grid(row=5, column=4, pady=(10, 0), padx=0, )
-        sel_btn7.grid(row=6, column=4, pady=(10, 0), padx=0, )
-
+        lab_img1.place(x= 138, y = 20)
+        sel_btn1.place(x=50,y=120)
+        sel_btn2.place(x=297,y=120)
+        sel_btn3.place(x=50,y=190)
+        sel_btn4.place(x=297,y=190)
+        sel_btn5.place(x=50,y=260)
+        sel_btn6.place(x=297, y=260)
+        sel_btn7.place(x=50, y=330)
+        
     def show_option():
         start_btn.destroy()
         lab_img.destroy()
         intro.destroy()
         option()
+        
+    
     
     main_window = Tk()
-
-    main_window.geometry("500x500+500+150")
+    main_window.geometry("500x500+300+100")
     main_window.resizable(0, 0)
     main_window.title("VUA TIẾNG VIỆT")
-    main_window.configure(background="#2a363b")
+  
+    photo0 = Image.open("bg.png")
+    resize = photo0.resize((500,500), Image.ANTIALIAS)
+    photo1=ImageTk.PhotoImage(resize)
+    bg_label = Label(main_window, image = photo1)
+    bg_label.place(x=0, y = 0, relwidth = 1, relheight = 1)
+
+
+    img = PhotoImage(file='back.png')
+    
+    def dong():
+        main_window.destroy()
+    img_exit = Image.open("thoat.png")
+    resize = img_exit.resize((50,50), Image.ANTIALIAS)
+    exit=ImageTk.PhotoImage(resize)
+    lab_exit = Button(
+        main_window,
+        image = exit,
+        bg='#312D4B',
+        borderwidth=0,
+        command = dong,
+    )    
+    lab_exit.pack( anchor='nw', pady=3, padx=7)
+    
+    logo1 = Image.open("logo1.png")
+    resize = logo1.resize((200,102), Image.ANTIALIAS)
+    photo3=ImageTk.PhotoImage(resize)
     
 
-    img = PhotoImage(file="back.png")
 
     lab_img = Label(
-        main_window,
-        text="VUA TIẾNG VIỆT",
-        bg='#dfa801',
-        font=("Titillium 15 bold", 30)
+    main_window,
+    image = photo3,
+    fg ='#000000',
+    bg='#312D4B',
+    
     )
-    lab_img.pack(pady=(50, 0))
+    lab_img.place(x= 179, y = 7)
+
+    
 
     start_btn = Button(
         main_window,
         text="BẮT ĐẦU",
-        width=18,
-        borderwidth=8,
+        width=15,
+        borderwidth=5,
         fg="#000000",
-        bg="#dfa801",
+        bg="#FED329",
         font=("", 13),
         cursor="hand2",
         command=show_option,
     )
-    start_btn.pack(pady=(150,20))
+    start_btn.pack(pady=(340,20))
     def show_intro():
         messagebox.showinfo("HƯỚNG DẪN", "NGƯỜI CHƠI CÓ 15 GIÂY ĐỂ TRẢ LỜI \n \n MỖI CÂU TRẢ LỜI ĐÚNG ĐƯỢC 5 ĐIỂM \n \n MỖI LẦN DÙNG GỢI Ý BỊ TRỪ ĐI 1 ĐIỂM \n")
         
     intro=Button(
         text="HƯỚNG DẪN",
-        width=18,
-        borderwidth=8,
+        width=15,
+        borderwidth=5,
         fg="#000000",
-        bg="#dfa801",
+        bg="#FED329",
         font=("", 13),
         command=show_intro,
     )
     intro.pack(pady=(0,0))
-
+    
     pygame.mixer.init()
     pygame.mixer.music.load("1.mp3")
     pygame.mixer.music.play(loops=100)
+
+
+    # def play():
+    #     pygame.mixer.music.load("1.mp3")
+    #     pygame.mixer.music.play(loops=100)
+    # def stop():
+    #     pygame.mixer.music.stop()
+    # global is_on
+    # is_on = True
+    # def switch():
+    #     global is_on
+        
+    #     if is_on:
+    #         on_button.config(image = img_off)
+    #         pygame.mixer.music.load("1.mp3")
+    #         pygame.mixer.music.play(loops=100)
+    #         stop()
+    #         is_on = False
+    #     else:
+    #         on_button.config(image = img_on)
+    #         is_on = True    
+    #         play()
     
+    # on = Image.open("loabat.png")
+    # resize1 = on.resize((30,30), Image.ANTIALIAS)
+    # img_on=ImageTk.PhotoImage(resize1)
+    # off = Image.open("loatat.png")
+    # resize2 = off.resize((35,35), Image.ANTIALIAS)
+    # img_off=ImageTk.PhotoImage(resize2)
+    # on_button = Button(
+    #     main_window,
+    #     image=img_on,
+    #     bg='#312D4B',
+    #     border=0,
+    #     justify='center',
+    #     command=switch,
+    # )
+    # on_button.place(x=40,y=5)
+    # off_button = Button(
+    #     main_window,
+    #     image=img_off,
+    #     bg='#312D4B',
+    #     border=0,
+    #     justify='center',
+    #     command=switch,
+    # )
+    # on_button.place( anchor='nw', x=7, y =50)
+        
     main_window.mainloop()
 
 
